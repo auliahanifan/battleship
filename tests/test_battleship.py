dad5287c86cf45a65eba7c60cf2122cb2612d3da
Aulia Rahman Hanifan <aulia.hanifan@traveloka.com>
2021-09-01 06:48:14 +0700

    add test battleship

---
+++ tests/test_battleship.py
@@ -0,0 +1,16 @@
+
+from model.battleship import Battleship
+
+
+def test_get_damage_health_change(mocker):
+    sut = Battleship()
+    sut.get_damage(3)
+
+    assert sut.health == -2
+
+def test_get_damage_health_not_change(mocker):
+    sut = Battleship()
+    sut.get_damage(1)
+    sut.get_damage(20)
+
+    assert sut.health == 0
