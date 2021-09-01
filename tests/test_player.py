d42f4ff374a9cd2790f598e250797ea8cc62ed18
Aulia Rahman Hanifan <aulia.hanifan@traveloka.com>
2021-09-01 08:06:09 +0700

    add more test

--- tests/test_player.py
+++ tests/test_player.py
@@ -9,11 +9,27 @@ def test_get_current_def_battleground_map(mocker):
     ship = Battleship()
     def_battleground.set_objects(positions, ship)
 
+    sut = Player(def_battleground, None)
+    
+    assert def_battleground.show_map() == sut.get_current_def_battleground_map()
+
+def test_attack(mocker):
+    def_battleground = Battleground(2)
+    positions = [[1,1], [0, 1]]
+    ship = Battleship()
+    def_battleground.set_objects(positions, ship)
+
     att_battleground = Battleground(2)
     positions = [[1,1], [0, 1]]
     missile = Missile()
     att_battleground.set_objects(positions, missile)
 
-    player = Player(def_battleground, att_battleground)
-    
-    assert def_battleground.show_map() == player.get_current_def_battleground_map()
+    opponent = Player(def_battleground, None)
+    sut = Player(None, att_battleground)
+
+    sut.attack(opponent)
+
+    assert 0 == opponent.healths
+
+
+
